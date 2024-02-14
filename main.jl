cd8ef24bd3e23ced6fb10daacfae200b2b9f951a
Andrew Gray <andrew.gray@queensu.ca>
2024-02-14 17:00:03 -0500

    changed main.jl

--- main.jl
+++ main.jl
@@ -1,9 +1,10 @@
 # Example julia program that will be ran on HPC cluster
 
-add(x, y) = x + y
+add(x, y, z) = x + y + z
 
 x = 3
 y = 4
-z = add(x, y)
+z = 2
+w = add(x, y, z)
 
-println("$x + $y = $z")
+println("$x + $y + $z = $w")
